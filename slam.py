6a750324e6adfdf5e7c1319bd7258512325666fd
daniel.do@hotmail.com <daniel.do@hotmail.com>
2021-08-11 19:49:32 -0300

    improve track points

--- slam.py
+++ slam.py
@@ -2,19 +2,37 @@
 import sys
 import cv2
 from display import Display
+import numpy as np
 
 
 W = 2562//4
 H = 1440//4
 
-
-
 disp = Display(W, H)
-orb = cv2.ORB()
+
+class FeatureExtractor(object):
+	GX = 16//2
+	GY = 12//2
+
+	def __init__(self):
+		self.orb = cv2.ORB_create(100)
+
+	def extract(self, img):
+		feats = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance=3)
+		print(feats)
+		return feats
+
+
+fe = FeatureExtractor()
 
 def process_frame(img):
-	kp1, des1 = orb.detectAndCompute(img,None)
 	img = cv2.resize(img,(W,H))
+	kp = fe.extract(img)
+
+	for p in kp:
+		u,v = map(lambda x: int(round(x)), p[0])
+		cv2.circle(img, (u,v), color=(0,255,0), radius=3)
+
 	disp.paint(img)
 
 if __name__ == "__main__":
